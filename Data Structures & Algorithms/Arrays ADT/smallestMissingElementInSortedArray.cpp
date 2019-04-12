d4f3f5331cd157047b72ac5da6e6de7939f410d5
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-04-13 00:58:19 +0200

    Add files via upload

---
+++ Data Structures & Algorithms/Arrays ADT/smallestMissingElementInSortedArray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int smallestMissingElementInSortedArray(int A[],int n){
+	int k=0;
+	if(A[0]!=0){
+		return 0;
+	}else{
+		for (int i = 0; i < n; ++i)
+		{
+			if(A[i+1]-A[i]>1){
+				k=A[i]+1;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < n; ++i){
+		if(k==0){
+			return A[n-1]+1;
+		}
+	}
+	return k;
+}
+int main(int argc, char const *argv[])
+{
+	int A[]={1,1,2,3,4,5,7,8,10};
+	int n=sizeof(A)/sizeof(A[0]);
+	cout<<smallestMissingElementInSortedArray(A,n);
+	return 0;
+}
